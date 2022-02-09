0dd5c53cb584776b8ab9b63c7c441a68c8f67c65
Tim Heuer <tim@timheuer.com>
2022-02-08 20:20:43 -0800

    Add project files.

---
+++ VSIXProject1/Commands/MyCommand.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Text;
+
+namespace VSIXProject1
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await Package.JoinableTaskFactory.SwitchToMainThreadAsync();
+            DocumentView docView = await VS.Documents.GetActiveDocumentViewAsync();
+            if (docView?.TextView == null) return;
+            SnapshotPoint position = docView.TextView.Caret.Position.BufferPosition;
+            docView.TextBuffer?.Insert(position, Guid.NewGuid().ToString());
+        }
+    }
+}
